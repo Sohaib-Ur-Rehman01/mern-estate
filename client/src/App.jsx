307b27f04702cbfdc66d51df1731d5efcea5c75e
Sohaib-Ur-Rehman01 <sohaiburrehman65@gmail.com>
2025-10-08 04:46:13 -0700

    Project Setup

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import SignIn from "./pages/sing.in";
+import SignUp from "./pages/sign.up";
+import About from "./pages/about";
+import Profile from "./pages/profile";
+import Header from "./components/header";
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
