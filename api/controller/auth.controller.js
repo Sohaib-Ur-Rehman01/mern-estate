6f372b57caebbd1daa546768cab845296b873c83
Sohaib-Ur-Rehman01 <sohaiburrehman65@gmail.com>
2025-10-09 03:23:21 -0700

    create a middleware and a function to handle possibler errors

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
-export const signup = async (req, res) => {
+import { errorHandler } from '../utils/error.js';
+export const signup = async (req, res, next) => {
 
   console.log(req.body);
   const { username, email, password } = req.body
@@ -11,8 +12,7 @@ export const signup = async (req, res) => {
     res.status(201).json("User created successfully!")
 
   } catch (error) {
-    res.status(500).json(error)
-    console.log(error, "auth.controller.js file");
+    next(errorHandler(550, 'Error from the function errorHandler'))
 
   }
 
--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import e from 'express'
 dotenv.config()
 mongoose.connect(process.env.MONGO).then(() => {
   console.log("Connected to MongoDB");
@@ -19,3 +20,13 @@ app.listen(3000, () => {
 });
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500
+  const message = err.message || "Internal Server Error::: check the index.js file"
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  })
+})
