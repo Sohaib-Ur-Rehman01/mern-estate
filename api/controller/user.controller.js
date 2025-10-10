22f6811ed505eb0df2cd77fe3904e36cb5027dd9
Sohaib-Ur-Rehman01 <sohaiburrehman65@gmail.com>
2025-10-10 04:27:08 -0700

    add delete user functionality

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,6 +1,7 @@
 import bcryptjs from "bcryptjs";
 import User from '../models/user.model.js'
 import { errorHandler } from "../utils/error.js";
+import clearCookie from "cookie-parser"
 
 export const test = (req, res) => {
   res.json({
@@ -23,6 +24,16 @@ export const updateUser = async (req, res, next) => {
     }, { new: true })
     const { password, ...rest } = updatedUser._doc
     res.status(200).json(rest)
+  } catch (error) {
+    next(error)
+  }
+}
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id) return next(errorHandler(401, "You can only delete your account!"))
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    clearCookie('access_token')
+    res.status(200).json("User deleted successfully!")
   } catch (error) {
     next(error)
   }
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -33,6 +33,18 @@ const userSlice = createSlice({
       state.error = action.payload
       state.loading = false
     },
+    deleteUserStart: (state) => {
+      state.loading = true
+    },
+    deleteUserSuccess: (state, action) => {
+      state.currentUser = null
+      state.loading = false
+      state.error = null
+    },
+    deleteUserFailure: (state, action) => {
+      state.error = action.payload
+      state.loading = false
+    },
 
   },
 })
@@ -41,6 +53,9 @@ export const {
   signInSuccess,
   signInFailure, updateUserStart,
   updateUserSuccess,
-  updateUserFailure
+  updateUserFailure,
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess
 } = userSlice.actions
 export default userSlice.reducer
