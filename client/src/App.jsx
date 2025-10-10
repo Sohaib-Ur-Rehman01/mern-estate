e80a59ac8681afe7a65ee42155c3de0271fae079
Sohaib-Ur-Rehman01 <sohaiburrehman65@gmail.com>
2025-10-10 06:49:55 -0700

    complete create listing page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import About from "./pages/about";
 import Profile from "./pages/profile";
 import Header from "./components/header";
 import PrivateRoute from "./components/privateRoute";
+import CreateListing from "./pages/createListing";
 export default function App() {
   return (
     <BrowserRouter>
@@ -19,6 +20,7 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
+          <Route path="/create-listing" element={<CreateListing />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -8,6 +8,7 @@ import {
 } from "firebase/storage";
 import { app } from "../firebase";
 import React from "react";
+import { Link } from "react-router-dom";
 import {
   updateUserStart,
   updateUserFailure,
@@ -180,6 +181,12 @@ export default function Profile() {
         >
           {loading ? "Loading..." : "Update"}
         </button>
+        <Link
+          className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95"
+          to="/create-listing"
+        >
+          Create Listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span
