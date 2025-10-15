faf9e5b03a614ea884665ebae89019d150e628d9
Sohaib-Ur-Rehman01 <sohaiburrehman65@gmail.com>
2025-10-15 02:11:27 -0700

    complete about page

--- client/src/pages/about.jsx
+++ client/src/pages/about.jsx
@@ -1,5 +1,35 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Sahand Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Sahand Estate provides trusted real estate solutions, helping
+        individuals and families discover homes that match their needs and
+        lifestyle. With a focus on quality listings, transparency, and seamless
+        service, we make property searching simple, secure, and stress-free
+      </p>
+      <p className="mb-4 text-slate-700">
+        Sahand Estate is a reliable real estate platform dedicated to helping
+        people find homes that suit their lifestyle, budget, and future plans.
+        Whether you're looking to buy, sell, or rent, we offer a wide range of
+        properties with accurate details and updated listings. Our goal is to
+        make the search process convenient and transparent, so users can make
+        confident decisions. With a customer-focused approach and strong market
+        understanding, Sahand Estate simplifies real estate journeys and
+        connects clients with the right opportunities
+      </p>
+      <p className="mb-4 text-slate-700">
+        Our mission is to make real estate accessible, transparent, and
+        stress-free for everyone. We aim to connect people with the right
+        properties through trusted listings, clear information, and responsive
+        support. By combining user-friendly technology with market insight, we
+        strive to guide buyers, sellers, and renters toward confident decisions
+        and long-term satisfaction in their property journey
+      </p>
+    </div>
+  );
 }
